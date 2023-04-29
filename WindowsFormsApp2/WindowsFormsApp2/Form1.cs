56309d0e2f588c2a931309ba6020e7f79308dfcf
Mateo Roksandić <mroksandi21@student.foi.hr>
2023-04-29 09:06:31 +0200

    Create Form's labels, textboxes, button

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -16,5 +16,20 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
